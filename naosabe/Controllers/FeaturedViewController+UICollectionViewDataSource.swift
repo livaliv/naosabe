055e26744ffb1a132f52ccb8974f84f75ecab973
Liva <lclr@academy.cin.ufpe.br>
2022-07-13 14:54:51 -0300

    antes de mexer no storyboard

--- naosabe/Controllers/FeaturedViewController+UICollectionViewDataSource.swift
+++ naosabe/Controllers/FeaturedViewController+UICollectionViewDataSource.swift
@@ -17,26 +17,27 @@ extension FeaturedViewController: UICollectionViewDataSource {
         }
         
     }
-    
-    fileprivate func makeNowPlayingCell(_ indexPath: IndexPath) -> UICollectionViewCell {
+    fileprivate func makePopularCell(_ indexPath: IndexPath) -> PopularCollectionViewCell {
         if let cell =
-            nowPlayingCollectionView.dequeueReusableCell(withReuseIdentifier: "nowPlayingCell", for: indexPath) as? NowPlayingCollectionViewCell {
-            cell.titleLabel.text = nowPlayingMovies[indexPath.item].title
-            cell.dateLabel.text = nowPlayingMovies[indexPath.item].releaseDate
-            cell.imageView.image = UIImage(named: nowPlayingMovies[indexPath.item].poster)
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-    
-    fileprivate func makePopularCell(_ indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell =
-            popularCollectionView.dequeueReusableCell(withReuseIdentifier: "popularCell", for: indexPath) as? PopularCollectionViewCell {
+            popularCollectionView.dequeueReusableCell(withReuseIdentifier: PopularCollectionViewCell.cellIdentifier, for: indexPath) as? PopularCollectionViewCell {
             cell.titleLabel.text = popularMovies[indexPath.item].title
             cell.image.image = UIImage(named: popularMovies[indexPath.item].backdrop)
             return cell
         }
-        return UICollectionViewCell()
+        return PopularCollectionViewCell()
+    }
+    fileprivate func makeNowPlayingCell(_ indexPath: IndexPath) -> NowPlayingCollectionViewCell {
+        if let cell =
+            nowPlayingCollectionView.dequeueReusableCell(withReuseIdentifier: NowPlayingCollectionViewCell.cellIdentifier, for: indexPath) as? NowPlayingCollectionViewCell {
+            let titulo: String = nowPlayingMovies[indexPath.item].title
+            cell.titleLabel.text = titulo
+//            criar variavel simples com 2 clique = seleciona, da o duplo clique, aperta refactor e extrair p variavel
+            cell.dateLabel.text = "\(nowPlayingMovies[indexPath.item].releaseDate.prefix(4))"
+//            String = "\blabbla"
+            cell.imageView.image = UIImage(named: nowPlayingMovies[indexPath.item].poster)
+            return cell
+        }
+        return NowPlayingCollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
